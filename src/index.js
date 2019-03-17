6d3660a547f1f58896b0f77435a8bedc34422245
Aayush Chaubey <aayush.chaubey674@gmail.com>
2019-03-17 15:22:01 +0530

    Taking Turns in tic tac implemented

--- src/index.js
+++ src/index.js
@@ -10,80 +10,87 @@ import * as serviceWorker from './serviceWorker';
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
 
-class Square extends React.Component {
-    
-    render() {
-      return (
-        <button
-          className="square"
-          onClick={() => {this.props.onClick({ value: "X" });}}
-        >
-          {this.props.value}
-        </button>
-      );
-    }
-  }
-  
-  class Board extends React.Component {
-    constructor(props){
-        super(props);
-        this.state ={
-            squares : Array(9).fill(null),
-        }
-    }
 
-    handleClick(i){
-     const squares = this.state.squares.slice();
-     squares[i] = "X";
-     this.setState({sqaures: squares });
-    }
-    
-    renderSquare(i) {
-      return <Square value={this.state.squares[i]}
-      onClick = {() => this.handleClick(i)}/>;
-    }
-  
-    render() {
-      const status = 'Next player: X';
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
-    }
-  }
-  
-  class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
-    }
-  }
-  
+//square component
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
 
-  ReactDOM.render(<Game />, document.getElementById('root'));
+
+//Board Component consisting of Square as a child component
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext : true,
+    }
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  renderSquare(i) {
+    console.log("hey thwere" ,  this.state.squares[i]);
+    return <Square value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)} />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
+//Game Component consisting of Board as a child component
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+ReactDOM.render(<Game />, document.getElementById('root'));
